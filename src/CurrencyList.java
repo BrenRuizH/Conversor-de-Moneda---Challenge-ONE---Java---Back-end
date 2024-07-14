456e7806ee9fb85444c89994f38c91d419c9cb95
Brenda Ruiz <ruizhernandezbrendac@gmail.com>
2024-07-14 15:21:09 -0600

    2024/07/14 Clase Currency List

---
+++ src/CurrencyList.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class CurrencyList {
+    private List<Currency> currencies;
+
+    // Getters y Setters
+    public List<Currency> getCurrencies() {
+        return currencies;
+    }
+
+    public void setCurrencies(List<Currency> currencies) {
+        this.currencies = currencies;
+    }
+}
